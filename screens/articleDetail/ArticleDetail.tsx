587975d0f1c27500dc42af62f68b4f2169c9e535
Suraj Shrestha <syuraj@gmail.com>
2022-01-15 19:35:51 -0500

    use tailwind css to style components

--- screens/articleDetail/ArticleDetail.tsx
+++ screens/articleDetail/ArticleDetail.tsx
@@ -1,12 +1,16 @@
+import clsx from 'clsx'
 import React from 'react'
 import { ScrollView, View } from 'react-native'
 import { Avatar, Button, Card, Paragraph, Text } from 'react-native-paper'
+import { useTheme } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'tailwind-react-native-classnames'
 import { getRelativeTime } from '../home/ArticleListItem'
 
 const ArticleDetail = (props) => {
 	const { navigation } = props
 	const { article } = props.route.params
+	const { colors } = useTheme()
 
 	const handleLinkClick = () => {
 		const link = article.url
@@ -25,10 +29,12 @@ const ArticleDetail = (props) => {
 						left={() => <Avatar.Image size={40} source={{ uri: article.source.logoLink }} />}
 					/>
 					<Card.Content>
-						<View>{createContentView(article.original_content, article.translated_content)}</View>
+						<View>{createContentView(article.original_content, article.translated_content, colors)}</View>
 					</Card.Content>
 					<Card.Actions>
-						<Button onPress={handleLinkClick}>{'Read Original Source'}</Button>
+						<Button onPress={handleLinkClick} color={colors.notification}>
+							{'Read Original Source'}
+						</Button>
 					</Card.Actions>
 				</Card>
 			</ScrollView>
@@ -36,14 +42,14 @@ const ArticleDetail = (props) => {
 	)
 }
 
-const createContentView = (original_content, translated_content) => {
+const createContentView = (original_content, translated_content, colors) => {
 	return (
 		<View>
 			{original_content.map((item, index) => {
 				return (
 					<View key={index}>
-						<Text>{translated_content[index]}</Text>
-						<Paragraph>{item}</Paragraph>
+						<Text style={{ color: colors.primary }}>{translated_content[index]}</Text>
+						<Paragraph style={tw`mb-3`}>{item}</Paragraph>
 					</View>
 				)
 			})}
--- screens/home/ArticleListItem.tsx
+++ screens/home/ArticleListItem.tsx
@@ -1,21 +1,27 @@
 import moment from 'moment'
 import React from 'react'
-import { Avatar, Card, Paragraph } from 'react-native-paper'
+import { TouchableRipple, Avatar, Card, Paragraph } from 'react-native-paper'
+import tw from 'tailwind-react-native-classnames'
 
 const ArticleListItem = ({ navigation, article }) => {
 	return (
-		<Card onPress={() => navigation.navigate('ArticleDetail', { article })}>
-			<Card.Cover source={{ uri: article.lead_image_url }} />
-			<Card.Title
-				title={article.title}
-				subtitle={getRelativeTime(article.date_published || article.date_modified)}
-				titleNumberOfLines={4}
-				left={() => <Avatar.Image size={40} source={{ uri: article.source.logoLink }} />}
-			/>
-			<Card.Content>
-				<Paragraph>{article.translated_content[0].substring(0, 50) + '...'}</Paragraph>
-			</Card.Content>
-		</Card>
+		<TouchableRipple
+			onPress={() => navigation.navigate('ArticleDetail', { article })}
+			rippleColor="rgba(0, 0, 0, .32)"
+		>
+			<Card style={tw`mt-2 mb-2 ml-4 mr-4 rounded-lg`}>
+				<Card.Cover source={{ uri: article.lead_image_url }} />
+				<Card.Title
+					title={article.title}
+					subtitle={getRelativeTime(article.date_published || article.date_modified)}
+					titleNumberOfLines={4}
+					left={() => <Avatar.Image size={40} source={{ uri: article.source.logoLink }} />}
+				/>
+				<Card.Content>
+					<Paragraph>{article.translated_content[0].substring(0, 50) + '...'}</Paragraph>
+				</Card.Content>
+			</Card>
+		</TouchableRipple>
 	)
 }
 
