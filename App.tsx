b067f6eba25e5bbd0f420404ee42e25d52c61638
Suraj Shrestha <syuraj@gmail.com>
2022-01-16 10:58:05 -0500

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import SplashScreen from 'react-native-splash-screen'
 import HomeScreen from './screens/home/Home'
 import ArticleDetailScreen from './screens/articleDetail/ArticleDetail'
 import ArticleWeb from './screens/articleDetail/ArticleWeb'
@@ -8,6 +9,10 @@ import ArticleWeb from './screens/articleDetail/ArticleWeb'
 const Stack = createNativeStackNavigator()
 
 const App = () => {
+	useEffect(() => {
+		SplashScreen.hide()
+	}, [])
+
 	return (
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: true }}>
--- android/app/src/main/java/com/siristechnology/spanilla/MainActivity.java
+++ android/app/src/main/java/com/siristechnology/spanilla/MainActivity.java
@@ -2,11 +2,13 @@ package com.siristechnology.spanilla;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 
